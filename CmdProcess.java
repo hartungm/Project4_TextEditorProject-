74776b258c3901b77cb38e7fff241dd042488086
matthewarmand <marmand68@gmail.com>
2014-04-04 10:22:25 -0400

    Help method completed, save method modified slightly

--- CmdProcess.java
+++ CmdProcess.java
@@ -45,11 +45,15 @@ public class CmdProcess {
 		PrintWriter p = null;
 		try {
 			p=new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+			Line tmp = tedList.getHead();
+			while (tmp!=null){
+				p.println(tmp);
+				tmp=tmp.getNext();
+			}
+			p.close();
 		} catch (IOException e) {
 			System.out.println("Error while writing file!");
 		}
-		p.println(tedList.toString());
-		p.close();
 	}
 
 	public void loadFile(String fileName) {
@@ -70,7 +74,31 @@ public class CmdProcess {
 	}
 
 	public void showHelp() {
-
+		//non-implemented functionality included, commented out
+		String out = "";
+		out += ("Welcome to Text Editor Help!\n");
+		out += ("Command:   Function:\n");
+		out += ("b sentence Insert sentence before current");
+		out += (" line, make inserted line current\n");
+		out += ("i sentence Insert sentence after current");
+		out += (" line, make inserted line current\n");
+		out += ("m          Move cursor down 1 position\n");
+	  //out += ("m #        Move cursor down # positions\n");
+		out += ("u          Move cursor up 1 position\n");
+	  //out += ("u #		Move cursor down # positions\n");
+		out += ("r          Remove current line. Next line becomes ");
+		out += ("current, unless no next line, then previous becomes ");
+		out += ("current\n");
+	  //out += ("r #        Remove # lines, starting at current\n")
+		out += ("d          Display all lines with line numbers\n");
+	  //out += ("d # *      Display lines # to * with line numbers\n");
+		out += ("c          Clear all lines in the file\n");
+		out += ("s filename Save contents to specified text file\n");
+		out += ("l filename Load contents of file into current buffer\n");
+		out += ("h          Display this help page\n");
+		out += ("x          Exit the editor");
+		//extra functionality: only if saved to l, c, x
+		System.out.println(out);
 	}
 
 	public void exitEditor() {
