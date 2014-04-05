6d74fd362b30971ed197f6adde4c797387dcfc36
Michael Hartung <hartungm@mail.gvsu.edu>
2014-04-05 17:51:45 -0400

    added down and up for multiple lines
    Added the down and up methods for multiple line moves, also cleaned up
    the help method so that it looked nicer when pulled up in the terminal.

--- CmdProcess.java
+++ CmdProcess.java
@@ -20,10 +20,23 @@ public class CmdProcess {
 		tedList.insertAfter(newLine);
 	}
 
+	public void down(int numDown) {
+		while(numDown > 0) {
+			downOnePos();
+			numDown--;
+		}
+	}
+
 	public void downOnePos() {
 		tedList.down();
 	}
 
+	public void up(int numUp) {
+		while(numUp > 0) {
+			upOnePos();
+			numUp--;
+		}
+	}
 	public void upOnePos() {
 		tedList.up();
 	}
@@ -75,31 +88,27 @@ public class CmdProcess {
 		String out = "";
 		out += ("Welcome to Text Editor Help!\n");
 		out += ("Command:   Function:\n");
-		out += ("b sentence Insert sentence before current");
+		out += ("b 'sentence' Insert sentence before current");
 		out += (" line, make inserted line current\n");
-		out += ("i sentence Insert sentence after current");
+		out += ("i 'sentence' Insert sentence after current");
 		out += (" line, make inserted line current\n");
-		out += ("m          Move cursor down 1 position\n");
-	  //out += ("m #        Move cursor down # positions\n");
-		out += ("u          Move cursor up 1 position\n");
-	  //out += ("u #		Move cursor down # positions\n");
-		out += ("r          Remove current line. Next line becomes ");
-		out += ("current, unless no next line, then previous becomes ");
+		out += ("m            Move cursor down 1 position\n");
+	  //out += ("m #          Move cursor down # positions\n");
+		out += ("u            Move cursor up 1 position\n");
+	  //out += ("u #		  Move cursor down # positions\n");
+		out += ("r            Remove current line. Next line becomes ");
+		out += ("current, unless\n 	     no next line, then previous becomes ");
 		out += ("current\n");
-	  //out += ("r #        Remove # lines, starting at current\n")
-		out += ("d          Display all lines with line numbers\n");
-	  //out += ("d # *      Display lines # to * with line numbers\n");
-		out += ("c          Clear all lines in the file\n");
-		out += ("s filename Save contents to specified text file\n");
-		out += ("l filename Load contents of file into current buffer\n");
-		out += ("h          Display this help page\n");
-		out += ("x          Exit the editor");
+	  //out += ("r #          Remove # lines, starting at current\n")
+		out += ("d            Display all lines with line numbers\n");
+	  //out += ("d # *        Display lines # to * with line numbers\n");
+		out += ("c            Clear all lines in the file\n");
+		out += ("s 'filename' Save contents to specified text file\n");
+		out += ("l 'filename' Load contents of file into current buffer\n");
+		out += ("h            Display this help page\n");
+		out += ("x            Exit the editor");
 		//extra functionality: only if saved to l, c, x
 		System.out.println(out);
-	}
-
-	public void exitEditor() {
-		System.exit(1);
 	}
 
 	public void cutSelection() {
