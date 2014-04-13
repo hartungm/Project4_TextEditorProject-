e8f3b957b3450366a9d738c176d7bd2bc74f3702
Michael Hartung <hartungm@mail.gvsu.edu>
2014-04-13 01:53:27 -0400

    Added Preliminary JUnit Test File
    Added all the test cases we should need, they just need to be filled in.

---
+++ EditorTest.java
@@ -0,0 +1,95 @@
+/**
+* 	Editor Test Class
+*	Unit Tests for Project 4 of Winter 2014 CIS 163
+*	@author Michael Hartung, Matthew Armand
+*/
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class EditorTest {
+
+	@Test
+	public void testInsertAfter() {
+
+	}
+
+	@Test
+	public void testInsertBefore() {
+
+	}
+
+	@Test
+	public void testMoveUp() {
+
+	}
+
+	@Test
+	public void testMoveUpMult() {
+
+	}
+
+	@Test
+	public void testMoveDown() {
+
+	}
+
+	@Test
+	public void testMoveDownMult() {
+
+	}
+
+	@Test
+	public void testRemove() {
+
+	}
+
+	@Test
+	public void testRemoveMult() {
+
+	}
+
+	@Test
+	public void testDisplay() {
+
+	}
+
+	@Test
+	public void testDisplaySpecific() {
+
+	}
+
+	@Test
+	public void testClear() {
+
+	}
+
+	@Test
+	public void testSave() {
+
+	}
+
+	@Test
+	public void testLoad() {
+
+	}
+
+	@Test
+	public void testHelp() {
+
+	}
+
+	@Test
+	public void testExit() {
+
+	}
+
+	@Test
+	public void testCut() {
+
+	}
+
+	@Test
+	public void testPaste() {
+
+	}
+}
