78373750a8d069488eb33ffabd37f01a72c90cbf
Michael Hartung <hartungm@mail.gvsu.edu>
2014-04-14 15:04:19 -0400

    Added Tests to EditorTest
    Completed All tests except for tests for save,load,help, exit, cut, and
    paste. I wasn’t able to test whether these actually work or not, but i
    don’t see any issues with it.

--- EditorTest.java
+++ EditorTest.java
@@ -11,57 +11,119 @@ public class EditorTest {
 
 	@Test
 	public void testInsertAfter() {
-
+		Editor e = new Editor();
+		e.processCommand("i Test 1");
+		e.processCommand("i Test 2");
+		assertEquals("    Test 1\n--> Test 2\n", e.process.tedList.toString());
 	}
 
 	@Test
 	public void testInsertBefore() {
+		Editor e = new Editor();
+		e.processCommand("b Test 1");
+		e.processCommand("b Test 2");
+		assertEquals("--> Test 2\n    Test 1\n", e.process.tedList.toString());
+	}
 
+	@Test
+	public void testInsertLast() {
+		Editor e = new Editor();
+		e.processCommand("b Test 1");
+		e.processCommand("b Test 2");
+		e.processCommand("e Test 3");
+		assertEquals("    Test 2\n    Test 1\n--> Test 3\n", e.process.tedList.toString());
 	}
 
 	@Test
 	public void testMoveUp() {
-
+		Editor e = new Editor();
+		e.processCommand("b Test 1");
+		e.processCommand("b Test 2");
+		e.processCommand("e Test 3");
+		e.processCommand("u");
+		assertEquals("Test 1\n", e.getCurrentLine());
 	}
 
 	@Test
 	public void testMoveUpMult() {
-
+		Editor e = new Editor();
+		e.processCommand("b Test 1");
+		e.processCommand("b Test 2");
+		e.processCommand("e Test 3");
+		e.processCommand("u 2");
+		assertEquals("Test 2\n", e.getCurrentLine());
 	}
 
 	@Test
 	public void testMoveDown() {
-
+		Editor e = new Editor();
+		e.processCommand("i Test 1");
+		e.processCommand("b Test 2");
+		e.processCommand("b Test 3");
+		e.processCommand("b Test 4");
+		e.processCommand("m");
+		assertEquals("Test 3\n", e.getCurrentLine());
 	}
 
 	@Test
 	public void testMoveDownMult() {
-
+		Editor e = new Editor();
+		e.processCommand("i Test 1");
+		e.processCommand("b Test 2");
+		e.processCommand("b Test 3");
+		e.processCommand("b Test 4");
+		e.processCommand("m 2");
+		assertEquals("Test 2\n", e.getCurrentLine());
 	}
 
 	@Test
 	public void testRemove() {
-
+		Editor e = new Editor();
+		e.processCommand("i Test 1");
+		e.processCommand("i Test 2");
+		e.processCommand("i Test 3");
+		e.processCommand("r");
+		assertEquals("    Test 1\n--> Test 2", e.process.tedList.toString());
 	}
 
 	@Test
 	public void testRemoveMult() {
-
+		Editor e = new Editor();
+		e.processCommand("i Test 1");
+		e.processCommand("i Test 2");
+		e.processCommand("i Test 3");
+		e.processCommand("r 2");
+		assertEquals("--> Test 1\n", e.process.tedList.toString());
 	}
 
 	@Test
 	public void testDisplay() {
-
+		Editor e = new Editor();
+		e.processCommand("i Test 1");
+		e.processCommand("i Test 2");
+		e.processCommand("i Test 3");
+		e.processCommand("d");
+		assertEquals("    Test 1\n    Test 2\n--> Test 3\n", e.process.tedList.toString());
 	}
 
 	@Test
 	public void testDisplaySpecific() {
-
+		Editor e = new Editor();
+		e.processCommand("i Test 1");
+		e.processCommand("i Test 2");
+		e.processCommand("i Test 3");
+		e.processCommand("d 2 3");
+		assertEquals("    Test 2\n--> Test 3\n", e.process.tedList.toString());
 	}
 
 	@Test
 	public void testClear() {
-
+		Editor e = new Editor();
+		e.processCommand("i Test 1");
+		e.processCommand("i Test 2");
+		e.processCommand("i Test 3");
+		e.processCommand("c");
+		assertEquals("", e.process.tedList.toString());
 	}
 
 	@Test
@@ -76,7 +138,7 @@ public class EditorTest {
 
 	@Test
 	public void testHelp() {
-
+		
 	}
 
 	@Test
